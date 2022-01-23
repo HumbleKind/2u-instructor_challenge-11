6d260810563af1ddb260463fdc54a23e4aab5f87
Christian Sinai <thesinais@Christians-MacBook-Pro.local>
2022-01-23 11:16:23 -0800

    updated env var

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { animals } = require('./data/animals');
 
+const PORT = process.env.PORT || 3001;
+
 // instantiate the express server
 const app = express();
 
@@ -42,6 +44,6 @@ app.get('/api/animals', (req, res) => {
 })
 
 // chain 'listen' method on to express app instance
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`);
 })
